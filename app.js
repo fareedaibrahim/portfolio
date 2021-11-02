6bb35ba4d2089d71a2337ec0425492c00e8ca9f3
fareedaibrahim <daniyafarida@gmail.com>
2021-11-02 15:47:39 +0100

    Add  mobile responsiveness

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const menuToggler = document.querySelector('.lines');
+    const menu = document.querySelector('.right-section');
     const links = document.querySelectorAll('.link');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    menuToggler.addEventListener('click', () => {
+        menu.classList.toggle("nav-active");
+        menuToggler.classList.toggle('lines-active');
+
+        for (navItem of navItems) {
+            navItem.classList.add('nav-item-visible');
+        }
+    });
 
     for (link of links) {
         link.addEventListener('click', function() {
